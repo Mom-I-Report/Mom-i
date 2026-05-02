22cec9d19ce975e2c1dbafbadc63ec3de1502889
jangsung0031 <jangsung0031@gmail.com>
2026-05-02 14:17:49 +0900

    feat/noh랑 합침

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        demo:  resolve(__dirname, 'demo.html'),
+        main: resolve(__dirname, 'index.html'),
+        demo: resolve(__dirname, 'demo.html'),
         admin: resolve(__dirname, 'admin.html'),
       },
     },
